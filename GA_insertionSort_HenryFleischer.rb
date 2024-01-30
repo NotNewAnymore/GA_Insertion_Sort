e4c8b119df87d618a16e6fb69a6e7996cbde6b3c
NotNewAnymore <Henryfleischer01@gmail.com>
2024-01-29 18:07:36 -0800

    Did assignment
    Mostly copied code from notes. I don't quite understand parts of this anymore, as I did the actual work weeks ago. But I procrastinated far too much, so I've got to get this in now.

--- GA_insertionSort_HenryFleischer.rb
+++ GA_insertionSort_HenryFleischer.rb
@@ -1,1 +1,43 @@
-puts "Test"
+def generaterandoms(total)
+  numbers = [0]
+  i = 0
+  while i < total
+    numbers << Random.rand(10000)   # << appends to the end of an array. We don't need to make an object to use the random class, although if I wanted to use a specific seed, I could.
+    i += 1
+  end
+  numbers
+end
+
+class Array #Modify the Array class
+  def insertionSort #Method
+    i = 0
+    self.count.times { #Outer loop. Happens once per loop.
+      i += 1
+      j = i - 1
+      key = self[i]
+      if !self[j].nil? && !key.nil?
+        while j >= 0 && self[j] > key #Inner loop, move item across until it's sorted
+          self[j + 1] = self[j] #Move element
+          j = j - 1 #Move to previous element
+        end
+      end
+      self[j + 1] = key
+    }
+        self.pop()  #Remove trailing nil
+  end
+end
+
+numbers = [0]
+numbers = generaterandoms(25)
+puts "Unsorted numbers, length ~25"
+puts numbers.to_s
+numbers.insertionSort()
+puts "Sorted numbers, length ~25"
+puts numbers.to_s
+
+numbers = generaterandoms(75)
+puts "Unsorted numbers, length ~75"
+puts numbers.to_s
+numbers.insertionSort()
+puts "Sorted numbers, length ~75"
+puts numbers.to_s
